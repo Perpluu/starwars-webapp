a66858cab5a096515c024a3effc3c5451235678d
Perp <95175264+Perpluu@users.noreply.github.com>
2023-04-07 10:30:26 +0200

    Update views.py

--- views.py
+++ views.py
@@ -16,9 +16,11 @@ async def read_number_of_planets():
 @app.post("/movie")
 async def get_movie_data(movie_id: int):
     response = requests.get(f"https://swapi.py4e.com/api/films/{movie_id}/")
+    if response.status_code != 200:
+        return {"error": f"Movie with ID {movie_id} not found."}
     movie_data = response.json()
-    people = [
-        requests.get(people_url).json().get("name")
-        for people_url in movie_data.get("characters", [])
+    actors = [
+        requests.get(actor_url).json().get("name")
+        for actor_url in movie_data.get("characters", [])
     ]
-    return {"movie_name": movie_data.get("title"), "actors": people}
+    return {"movie_name": movie_data.get("title"), "actors": actors}
